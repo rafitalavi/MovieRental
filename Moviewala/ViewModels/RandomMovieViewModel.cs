b60b760654054954ae6a2a217f0b2244a60e7572
rafitalavi <rafitalavi12345@gmail.com>
2025-01-22 00:45:46 +0600

    Solved of CustomerTypes Migrations

--- Moviewala/ViewModels/RandomMovieViewModel.cs
+++ Moviewala/ViewModels/RandomMovieViewModel.cs
@@ -4,7 +4,7 @@ namespace Moviewala.ViewModels
 {
     public class RandomMovieViewModel
     {
-           public Movie Movie { get; set; }
+           public Movie? Movie { get; set; }
         public List<Customer> Customers { get; set; }    
     }
 }
